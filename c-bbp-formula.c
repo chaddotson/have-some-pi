f0c00d67adb59a36dd5068908289a7526cee03eb
Chad Dotson <chad@cdotson.com>
2015-11-11 00:06:12 -0600

    Fix line endings.

--- c-bbp-formula.c
+++ c-bbp-formula.c
@@ -1,1 +1,20 @@
-// compile with: gcc c-bbp-formula.c -o c-bbp-formula// https://en.wikipedia.org/wiki/Bailey%E2%80%93Borwein%E2%80%93Plouffe_formula#include <stdio.h>#include <math.h>int main(int argv, char **argc) {    int i = 0;    int numSteps = 11;    double piSummation = 0.0;    for(i = 0; i < numSteps; i++) {        piSummation += (1.0/(pow(16.0, (double)i))) * ((4.0/(8.0*i + 1.0)) - (2.0/(8.0*i + 4.0)) - (1.0/(8.0*i + 5.0)) - (1.0/(8.0*i + 6.0)));    }    printf("Pi = %.20f\n", piSummation);}
+// compile with: gcc c-bbp-formula.c -o c-bbp-formula
+
+// https://en.wikipedia.org/wiki/Bailey%E2%80%93Borwein%E2%80%93Plouffe_formula
+
+#include <stdio.h>
+#include <math.h>
+
+int main(int argv, char **argc) {
+    int i = 0;
+    int numSteps = 11;
+    double piSummation = 0.0;
+
+    for(i = 0; i < numSteps; i++) {
+        piSummation += (1.0/(pow(16.0, (double)i))) * ((4.0/(8.0*i + 1.0)) - (2.0/(8.0*i + 4.0)) - (1.0/(8.0*i + 5.0)) - (1.0/(8.0*i + 6.0)));
+    }
+
+    printf("Pi = %.20f\n", piSummation);
+}
+
+
