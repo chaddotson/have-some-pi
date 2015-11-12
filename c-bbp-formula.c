2130a6983dc3f6a509eaed146596d71c3e6a34a4
Chad Dotson <chad@cdotson.com>
2015-11-11 22:50:18 -0600

    Tweak print precisions to 15 places.  Accuracy is limited to 15 places since doubles only have 16 digits of precision.

--- c-bbp-formula.c
+++ c-bbp-formula.c
@@ -14,7 +14,7 @@ int main(int argv, char **argc) {
         piSummation += (1.0/(pow(16.0, (double)i))) * ((4.0/(8.0*i + 1.0)) - (2.0/(8.0*i + 4.0)) - (1.0/(8.0*i + 5.0)) - (1.0/(8.0*i + 6.0)));
     }
 
-    printf("Pi = %.20f\n", piSummation);
+    printf("Pi = %.15f\n", piSummation);
 }
 
 
--- python-bbp-formula.py
+++ python-bbp-formula.py
@@ -10,4 +10,4 @@ pi_summation = 0.0
 for i in range(0, num_steps):
     pi_summation += (1.0/math.pow(16.0, i)) * ((4.0/(8.0*i + 1.0)) - (2.0/(8.0*i + 4.0)) - (1.0/(8.0*i + 5.0)) - (1.0/(8.0*i + 6.0)))
 
-print("Pi = {0:.20f}".format(pi_summation))
+print("Pi = {0:.15f}".format(pi_summation))
